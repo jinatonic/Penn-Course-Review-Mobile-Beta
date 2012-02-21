97112b7ec2864991fb37821fb8805ff7f208641c
Jinyan Cao <jinyan.09.mlk@gmail.com>
2012-02-21 02:12:20 -0500

    Removed bin files

--- src/edu/upenn/cis/cis350/PCRBetaActivity.java
+++ src/edu/upenn/cis/cis350/PCRBetaActivity.java
@@ -1,16 +1,7 @@
 package edu.upenn.cis.cis350;
 
-import java.io.*;
-import java.net.*;
-import java.text.ParseException;
-import java.util.Iterator;
-
-import org.json.*;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
 
 public class PCRBetaActivity extends Activity {
 	/** Called when the activity is first created. */

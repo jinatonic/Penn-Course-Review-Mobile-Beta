38c25c39706751685f8142930714decd98090aa9
Charles <laserzone5@gmail.com>
2012-02-20 21:06:11 -0500

    test course retrieval

--- src/edu/upenn/cis/cis350/PCRBetaActivity.java
+++ src/edu/upenn/cis/cis350/PCRBetaActivity.java
@@ -1,7 +1,17 @@
 package edu.upenn.cis.cis350;
 
+import java.io.*;
+import java.net.*;
+import java.text.ParseException;
+import java.util.Iterator;
+
+import org.json.*;
+
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class PCRBetaActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +19,24 @@ public class PCRBetaActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Parser p = new Parser();
+        String s ="Hi";
+        try {
+			s = p.getReviewsForCourse("CIS277");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			s = "What";
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			s = "Huh";
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			s = "Why";
+			e.printStackTrace();
+		}
+        TextView title = (TextView)findViewById(R.id.test);
+    	title.setText(s);
     }
 }

6c2699696e6dcc224ec7108791e0064ca7350a66
Connie Ho <hoconnie@seas.upenn.edu>
2012-02-26 01:08:43 -0500

    added unit test, doesnt run yet..

--- src/edu/upenn/cis/cis350/tests/ParserTest.java
+++ src/edu/upenn/cis/cis350/tests/ParserTest.java
@@ -2,25 +2,61 @@ package edu.upenn.cis.cis350.tests;
 
 import junit.framework.TestCase;
 
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.upenn.cis.cis350.backend.Parser;
+
 public class ParserTest extends TestCase {
 
-	protected void setUp() throws Exception {
+	Parser parser;
+	
+	@Before
+	public void setUp() throws Exception {
 		super.setUp();
+		parser = new Parser();
 	}
 	
-	protected void test_retrieveJSONObject() {
-		// TODO: Add default JSON Object to server to test this
+	@Test
+	public void test_retrieveJSONObject() {
+		// These should return valid JsonObjects
+		JSONObject result = parser.retrieveJSONObject("cis121");
+		assertNotNull(result);
+		result = parser.retrieveJSONObject("cis553");
+		assertNotNull(result);
+		result = parser.retrieveJSONObject("stat430");
+		assertNotNull(result);
+		result = parser.retrieveJSONObject("enm321");
+		assertNotNull(result);
+		result = parser.retrieveJSONObject("econ001");
+		assertNotNull(result);
+		
+		// These should return null
+		result = parser.retrieveJSONObject("cis100");
+		assertNull(result);
+		result = parser.retrieveJSONObject("wtfisthis/cis121");
+		assertNull(result);
+		
+		// Invalid inputs
+		result = parser.retrieveJSONObject(null);
+		assertNull(result);
+		result = parser.retrieveJSONObject("");
+		assertNull(result);
 	}
 	
-	protected void test_getReviewsForCourse() {
+	@Test
+	public void test_getReviewsForCourse() {
 		// TODO
 	}
 	
-	protected void test_displayCourseReviews() {
+	@Test
+	public void test_displayCourseReviews() {
 		// TODO
 	}
 	
-	protected void test_storeReviews() {
+	@Test
+	public void test_storeReviews() {
 		// TODO
 	}
 

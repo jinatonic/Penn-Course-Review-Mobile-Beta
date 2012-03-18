04afa2ae42feef0eb8e72c14447bff3c31e6975d
unknown <Cynthia@Cynthia-PC.(none)>
2012-03-18 00:13:00 -0400

    changing strings

--- gen/edu/upenn/cis/cis350/display/R.java
+++ gen/edu/upenn/cis/cis350/display/R.java
@@ -42,8 +42,12 @@ public final class R {
         public static final int SEARCH_TERM=0x7f040006;
         public static final int app_name=0x7f040001;
         public static final int clear=0x7f040003;
+        public static final int course_quality=0x7f040008;
+        public static final int difficulty=0x7f04000a;
         public static final int enter=0x7f040002;
         public static final int hello=0x7f040000;
+        public static final int instructor=0x7f040007;
+        public static final int instructor_quality=0x7f040009;
         public static final int pcr=0x7f040004;
         public static final int search_text=0x7f040005;
     }

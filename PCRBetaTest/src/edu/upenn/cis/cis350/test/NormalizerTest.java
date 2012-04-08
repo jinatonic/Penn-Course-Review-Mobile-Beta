94e9fa405305e32c2393ae8be70b1b634dc26030
Jinyan Cao <jinyan.09.mlk@gmail.com>
2012-04-07 22:20:33 -0400

    finished modifying the tests

--- PCRBetaTest/src/edu/upenn/cis/cis350/test/NormalizerTest.java
+++ PCRBetaTest/src/edu/upenn/cis/cis350/test/NormalizerTest.java
@@ -1,27 +1,29 @@
 package edu.upenn.cis.cis350.test;
 
 import android.test.AndroidTestCase;
+import edu.upenn.cis.cis350.backend.Normalizer;
+import edu.upenn.cis.cis350.objects.KeywordMap.Type;
 
 public class NormalizerTest extends AndroidTestCase {
 	
 	public void test_normalize() {
-//		String output = Normalizer.normalize("cis121");
-//		assertEquals("cis-121", output);
-//		
-//		output = Normalizer.normalize("CD Murphy");
-//		assertEquals("cd murphy", output);
-//		
-//		output = Normalizer.normalize("CIS");
-//		assertEquals("cis", output);
-//		
-//		output = Normalizer.normalize("CIS-121");
-//		assertEquals("cis-121", output);
-//		
-//		// These should fail
-//		output = Normalizer.normalize("CIS-121cxzg");
-//		assertEquals(null, output);
-//		
-//		output = Normalizer.normalize("CS-32as");
-//		assertEquals(null, output);
+		String output = Normalizer.normalize("cis121", Type.COURSE);
+		assertEquals("cis-121", output);
+		
+		output = Normalizer.normalize("CD Murphy", Type.INSTRUCTOR);
+		assertEquals("cd murphy", output);
+		
+		output = Normalizer.normalize("CIS", Type.DEPARTMENT);
+		assertEquals("cis", output);
+		
+		output = Normalizer.normalize("CIS-121", Type.COURSE);
+		assertEquals("cis-121", output);
+		
+		// These should fail
+		output = Normalizer.normalize("CIS-121cxzg", Type.COURSE);
+		assertEquals(null, output);
+		
+		output = Normalizer.normalize("CS-32as", Type.COURSE);
+		assertEquals(null, output);
 	}
 }

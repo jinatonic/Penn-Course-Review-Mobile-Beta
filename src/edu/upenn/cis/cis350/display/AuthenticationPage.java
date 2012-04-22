073204e3172da0381599586ab0ce92e577f2236b
Jinyan Cao <jinyan.09.mlk@gmail.com>
2012-04-21 23:35:28 -0400

    auth working for 4.0 (using asynctask)

--- src/edu/upenn/cis/cis350/display/AuthenticationPage.java
+++ src/edu/upenn/cis/cis350/display/AuthenticationPage.java
@@ -24,6 +24,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -40,7 +41,8 @@ import edu.upenn.isc.fastPdfServiceClient.api.FpsPennGroupsHasMember;
 
 public class AuthenticationPage extends Activity {
 
-
+	Context context;
+	
 	private String path = "http://beta.penncoursereview.com/androidapp/auth/?serial=";
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -49,6 +51,8 @@ public class AuthenticationPage extends Activity {
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
 		setContentView(R.layout.authentication_page);
+		
+		context = this.getApplicationContext();
 
 		// Set font to Times New Roman
 		Typeface timesNewRoman = Typeface.createFromAsset(this.getAssets(),"fonts/Times_New_Roman.ttf");
@@ -92,104 +96,8 @@ public class AuthenticationPage extends Activity {
 	}
 
 	public void onAuthSerialButtonClick(View v) {
-
-		int count = 0;
-		String pennKey_response = "!ERROR";
-		while(count < 3 && (pennKey_response.equals("!ERROR") || pennKey_response.equals("HTTP Error 500")))
-		{
-			try {
-				//Stuff to make it accept all certificates
-				SchemeRegistry schemeRegistry = new SchemeRegistry();
-				schemeRegistry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
-				schemeRegistry.register(new Scheme("https", new EasySSLSocketFactory(), 443));
-
-				HttpParams params = new BasicHttpParams();
-				params.setParameter(ConnManagerPNames.MAX_TOTAL_CONNECTIONS, 30);
-				params.setParameter(ConnManagerPNames.MAX_CONNECTIONS_PER_ROUTE, new ConnPerRouteBean(30));
-				params.setParameter(HttpProtocolParams.USE_EXPECT_CONTINUE, false);
-				HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
-
-				ClientConnectionManager cm = new SingleClientConnManager(params, schemeRegistry);
-				//creates a new Http Client
-				HttpClient client = new DefaultHttpClient(cm, params);
-				String serialNumber = ((EditText)findViewById(R.id.authenticate_text)).getText().toString();
-				Log.w("serialNumber", serialNumber);
-				//sends a request to the serial website
-				HttpGet httpget = new HttpGet(path+serialNumber.trim());
-
-				// Execute the request
-				HttpResponse response = client.execute(httpget);
-				//gets the response
-				pennKey_response = EntityUtils.toString(response.getEntity());
-				Log.w("Response",pennKey_response);
-				if(pennKey_response.equals("!ERROR") || pennKey_response.equals("HTTP Error 500")){
-					Log.w("Error","trying again");
-					count++;
-					continue;
-
-				}
-				else if(pennKey_response.equals("!INVALID")){
-					Log.w("Invalid serial","Invalid serial");
-					Context context = getApplicationContext();
-					CharSequence text = "Invalid serial number, please enter again";
-
-					int duration = Toast.LENGTH_LONG;
-
-					Toast toast = Toast.makeText(context, text, duration);
-					((EditText)findViewById(R.id.authenticate_text)).setText("");
-					toast.show();
-				}
-
-				else{
-					boolean hasMember = new FpsPennGroupsHasMember().assignGroupName("penn:isc:ait:apps:pennCourseReview:groups:pennCourseReviewStudents").assignSubjectSourceId("pennperson").assignSubjectIdentifier(pennKey_response).executeReturnBoolean();
-					Log.w("boolean is",hasMember+"");
-					if(!hasMember){
-						Log.w("Invalid pennkey","invalid pennkey");
-						Context context = getApplicationContext();
-						CharSequence text = "Invalid PennKey, unable to authenticate";
-
-						int duration = Toast.LENGTH_LONG;
-
-						Toast toast = Toast.makeText(context, text, duration);
-						((EditText)findViewById(R.id.authenticate_text)).setText("");
-						toast.show();
-					}
-					else{
-						Log.w("success!","success!");
-						Context context = getApplicationContext();
-						CharSequence text = "Authenticated!";
-
-						int duration = Toast.LENGTH_LONG;
-
-						Toast toast = Toast.makeText(context, text, duration);
-						((EditText)findViewById(R.id.authenticate_text)).setText("");
-						toast.show();
-						goToStartPage();
-					}
-
-				}
-
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				Log.w("Error","should not be here");
-				e.printStackTrace();
-
-			}
-			catch (IllegalArgumentException e){
-				Log.w("Illegal Character","illegal character");
-				Context context = getApplicationContext();
-				CharSequence text = "Invalid Serial, please try again";
-
-				int duration = Toast.LENGTH_LONG;
-
-				Toast toast = Toast.makeText(context, text, duration);
-				((EditText)findViewById(R.id.authenticate_text)).setText("");
-				toast.show();
-			}
-
-			count++;
-		}
-
+		String serialNumber = ((EditText)findViewById(R.id.authenticate_text)).getText().toString();
+		new AuthKey().execute(serialNumber);
 	}
 
 
@@ -210,6 +118,107 @@ public class AuthenticationPage extends Activity {
 				setResult(Constants.RESULT_QUIT);
 				this.finish();
 			}
+		}
+	}
+	
+	class AuthKey extends AsyncTask<String, Integer, String> {
+
+		@Override
+		protected String doInBackground(String... arg0) {
+			String serialNumber = arg0[0];
+			
+			int count = 0;
+			String pennKey_response = "!ERROR";
+			while(count < 3 && (pennKey_response.equals("!ERROR") || pennKey_response.equals("HTTP Error 500")))
+			{
+				try {
+					//Stuff to make it accept all certificates
+					SchemeRegistry schemeRegistry = new SchemeRegistry();
+					schemeRegistry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+					schemeRegistry.register(new Scheme("https", new EasySSLSocketFactory(), 443));
+
+					HttpParams params = new BasicHttpParams();
+					params.setParameter(ConnManagerPNames.MAX_TOTAL_CONNECTIONS, 30);
+					params.setParameter(ConnManagerPNames.MAX_CONNECTIONS_PER_ROUTE, new ConnPerRouteBean(30));
+					params.setParameter(HttpProtocolParams.USE_EXPECT_CONTINUE, false);
+					HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+
+					ClientConnectionManager cm = new SingleClientConnManager(params, schemeRegistry);
+					//creates a new Http Client
+					HttpClient client = new DefaultHttpClient(cm, params);
+					
+					Log.w("serialNumber", serialNumber);
+					
+					//sends a request to the serial website
+					HttpGet httpget = new HttpGet(path+serialNumber.trim());
+
+					// Execute the request
+					HttpResponse response = client.execute(httpget);
+					//gets the response
+					pennKey_response = EntityUtils.toString(response.getEntity());
+					Log.w("Response",pennKey_response);
+					if(pennKey_response.equals("!ERROR") || pennKey_response.equals("HTTP Error 500")){
+						Log.w("Error","trying again");
+						count++;
+						continue;
+
+					}
+					else if(pennKey_response.equals("!INVALID")){
+						Log.w("Invalid serial","Invalid serial");
+						CharSequence text = "Invalid serial number, please enter again";
+
+						displayToast(text);
+					}
+
+					else{
+						boolean hasMember = new FpsPennGroupsHasMember().assignGroupName("penn:isc:ait:apps:pennCourseReview:groups:pennCourseReviewStudents").assignSubjectSourceId("pennperson").assignSubjectIdentifier(pennKey_response).executeReturnBoolean();
+						Log.w("boolean is",hasMember+"");
+						if(!hasMember){
+							Log.w("Invalid pennkey","invalid pennkey");
+							CharSequence text = "Invalid PennKey, unable to authenticate";
+
+							displayToast(text);
+						}
+						else{
+							Log.w("success!","success!");
+							CharSequence text = "Authenticated!";
+							
+							displayToast(text);
+							
+							goToStartPage();
+						}
+
+					}
+
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					Log.w("Error","should not be here");
+					e.printStackTrace();
+
+				}
+				catch (IllegalArgumentException e){
+					Log.w("Illegal Character","illegal character");
+					CharSequence text = "Invalid Serial, please try again";
+					
+					displayToast(text);
+				}
+
+				count++;
+			}
+			return "DONE";
+		}
+		
+		private void displayToast(final CharSequence text) {
+			AuthenticationPage.this.runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					int duration = Toast.LENGTH_LONG;
+
+					Toast toast = Toast.makeText(context, text, duration);
+					((EditText)findViewById(R.id.authenticate_text)).setText("");
+					toast.show();
+				}
+			});
 		}
 	}
 }
